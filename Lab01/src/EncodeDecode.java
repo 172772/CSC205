4a76b1ddac74471c5f7c5b11a4b19ea2ce3be480
Matt Andre <matt@mattandre.me>
2012-09-06 15:47:29 -0400

    Added skeleton for EncodeDecode
    Signed-off-by: Matt Andre <matt@mattandre.me>

---
+++ Lab01/src/EncodeDecode.java
@@ -0,0 +1,48 @@
+
+public class EncodeDecode {
+	
+	//------------------------------------------
+	private String[] originalList;
+	
+	//------------------------------------------
+	private String[] encodedList;
+	
+	//------------------------------------------
+	private String[] decodedList;
+	
+	//------------------------------------------
+	public void EncodeDecode(String[] originalList) {
+
+	}
+
+	//------------------------------------------
+	public String encode(String originalWord) {
+
+	}
+	
+	//------------------------------------------
+	public String decode(String codedWord) {
+
+	}
+	
+	//------------------------------------------
+	public char forwardMap(char ch) {
+
+	}
+	
+	//------------------------------------------
+	public char backMap(char ch) {
+
+	}
+	
+	//------------------------------------------
+	public String[] getEncodedList() {
+
+	}
+	
+	//------------------------------------------
+	public String[] getDecodedList() {
+
+	}
+}
+ 
