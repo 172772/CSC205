4db0fe3bcf84ed1f054b5d37ca2a59fb232f2638
Matt Andre <matt@mattandre.me>
2012-09-07 18:28:25 -0400

    Completed filling in code. All should be functioning.
    Signed-off-by: Matt Andre <matt@mattandre.me>

--- Lab01/src/EncodeDecode.java
+++ Lab01/src/EncodeDecode.java
@@ -1,52 +1,252 @@
+/**
+ * 
+ * Encodes and Decodes a collection of words.
+ * 
+ * @author Matt Andre, Jeffrey Fisher
+ * @version 1.0
+ */
 
 public class EncodeDecode {
 	
-	//------------------------------------------
+	//------------------------------------------------------------------------------------
+	
+	//Offset of the mapping of characters
+	private final static int OFFSET = 2;
+	
+	//------------------------------------------------------------------------------------
+	
+	//Input list of words
 	private String[] originalList;
 	
-	//------------------------------------------
+	//------------------------------------------------------------------------------------
+	
+	//Encoded list of words
 	private String[] encodedList;
 	
-	//------------------------------------------
+	//------------------------------------------------------------------------------------
+	
+	//Decoded list of words
 	private String[] decodedList;
 	
-	//------------------------------------------
+	//------------------------------------------------------------------------------------
+	
+    /**
+     * The constructor:
+     * Encodes and decodes the original list of words
+     * @param originalList: an array of Strings
+     */
+
 	public EncodeDecode(String[] originalList) {
+		
+		//Initialize the word lists
 		this.originalList = originalList;
+		this.encodedList = new String[this.originalList.length];
+		this.decodedList = new String[this.originalList.length];
+
+		//Loop through each word in the original list of words
+		for(int i = 0; i < this.originalList.length; i++){
+			
+			String originalWord = this.originalList[i];
+			
+			//Encode word and then decode the encoded word
+			String encodedWord = encode(originalWord);
+			String decodedWord = decode(encodedWord);
+			
+			//Store encoded and decoded words
+			this.encodedList[i] = encodedWord;
+			this.decodedList[i] = decodedWord;
+			
+		}
 	}
 
-	//------------------------------------------
+	//------------------------------------------------------------------------------------
+	
+    /**
+     * Encodes a word.
+     * @param originalWord: A string object
+     * @return String: result of encoding
+     */
+	
 	public String encode(String originalWord) {
+		
+		String codedWord = new String();
+		
+		//Loop through each character in the word
+		for(int i = 0; i < originalWord.length(); i++){
+			
+			//Forward Map each character and add it to the the encoded word
+			codedWord += forwardMap(originalWord.charAt(i));
+			
+		}
 
-		return " ";
+		return codedWord;
 	}
 	
-	//------------------------------------------
+	//------------------------------------------------------------------------------------
+	
+    /**
+     * Decodes a word.
+     * @param codedWord: A string object
+     * @return String: result of decoding
+     */
+	
 	public String decode(String codedWord) {
 		
-		return " ";
-	}
-	
-	//------------------------------------------
-	public char forwardMap(char ch) {
+		String decodedWord = new String();
 		
-		return 'a';
+		//Loop through each character in the word
+		for(int i = 0; i < codedWord.length(); i++){
+			
+			//Back Map each character and add it to the the decoded word
+			decodedWord += backMap(codedWord.charAt(i));
+			
+		}
+
+		return decodedWord;
 	}
 	
-	//------------------------------------------
-	public char backMap(char ch) {
+	//------------------------------------------------------------------------------------
+	
+    /**
+     * Maps a character that is a letter or number forward by offset.
+     * Non-numbers and non-letters map to themselves.
+     * @param originalCharacter: A char
+     * @return char: result of mapping
+     */
+	
+	public char forwardMap(char originalCharacter) {
 		
-		return 'a';
+		//Set coded character to original in case it is not mapped
+		char codedCharacter = originalCharacter;
+		
+		//Check if character is a lower case letter
+		if(originalCharacter >= 'a' && originalCharacter <= 'z'){
+			
+			//Get current position of character
+			int currentAlphabeticPosition = originalCharacter - 'a';
+			
+			//Get the coded position by offsetting the current position
+			int codedAlphabeticPosition = ( ( currentAlphabeticPosition + OFFSET ) % 26 ) + (int)'a';
+			
+			//Get the character at the coded position
+			codedCharacter = (char)codedAlphabeticPosition;
+			
+		}
+		
+		//Check if character is a lower case letter
+		if(originalCharacter >= 'A' && originalCharacter <= 'Z'){
+			
+			//Get current position of character
+			int currentAlphabeticPosition = originalCharacter - 'A';
+			
+			//Get the coded position by offsetting the current position
+			int codedAlphabeticPosition = ( ( currentAlphabeticPosition + OFFSET ) % 26 ) + (int)'A';
+			
+			//Get the character at the coded position
+			codedCharacter = (char)codedAlphabeticPosition;
+			
+		}
+		
+		//Check if character is a number
+		if(originalCharacter >= '0' && originalCharacter <= '9'){
+			
+			//Get current position of character
+			int currentNumericPosition = originalCharacter - '0';
+			
+			//Get the coded position by offsetting the current position
+			int codedNumericPosition = ( ( currentNumericPosition + OFFSET ) % 10 ) + (int)'0';
+			
+			//Get the character at the coded position
+			codedCharacter = (char)codedNumericPosition;
+			
+		}
+		
+		return codedCharacter;
 	}
 	
-	//------------------------------------------
+	//------------------------------------------------------------------------------------
+	
+    /**
+     * Maps a character that is a letter or number backward by offset.
+     * Non-numbers and non-letters map to themselves.
+     * @param codedCharacter: A char
+     * @return char: result of mapping
+     */
+	
+	public char backMap(char codedCharacter) {
+		
+		//Set decoded character to coded in case it is not mapped
+		char decodedCharacter = codedCharacter;
+		
+		//Check if character is a lower case letter
+		if(codedCharacter >= 'a' && codedCharacter <= 'z'){
+			
+			//Get current position of character
+			int codedAlphabeticPosition = codedCharacter - 'a';
+			
+			//Get the decoded position by offsetting the code position
+			int decodedAlphabeticPosition = ( ( codedAlphabeticPosition + (26 - OFFSET) ) % 26 ) + (int)'a';
+			
+			//Get the character at the decoded position
+			decodedCharacter = (char)decodedAlphabeticPosition;
+			
+		}
+		
+		//Check if character is a lower case letter
+		if(codedCharacter >= 'A' && codedCharacter <= 'Z'){
+			
+			//Get current position of character
+			int codedAlphabeticPosition = codedCharacter - 'A';
+			
+			//Get the decoded position by offsetting the code position
+			int decodedAlphabeticPosition = ( ( codedAlphabeticPosition + (26 - OFFSET) ) % 26 ) + (int)'A';
+			
+			//Get the character at the decoded position
+			decodedCharacter = (char)decodedAlphabeticPosition;
+			
+		}
+		
+		//Check if character is a number
+		if(codedCharacter >= '0' && codedCharacter <= '9'){
+			
+			//Get current position of character
+			int codedNumericPosition = codedCharacter - '0';
+			
+			//Get the decoded position by offsetting the code position
+			int decodedNumericPosition = ( ( codedNumericPosition + (10 - OFFSET) ) % 10 ) + (int)'0';
+			
+			//Get the character at the decoded position
+			decodedCharacter = (char)decodedNumericPosition;
+			
+		}
+		
+		return decodedCharacter;
+	}
+	
+	//------------------------------------------------------------------------------------
+	
+    /**
+     * Gets the encoded list of words.
+     * @return String[]: result encoding original list of words
+     */
+	
 	public String[] getEncodedList() {
+		
 		return encodedList;
+		
 	}
 	
-	//------------------------------------------
+	//------------------------------------------------------------------------------------
+	
+    /**
+     * Gets the decoded list of words.
+     * @return String[]: result decoding encoded list of words
+     */
+	
 	public String[] getDecodedList() {
+		
 		return decodedList;
+		
 	}
 }
  
--- Lab01/src/EncodeDecodeTester.java
+++ Lab01/src/EncodeDecodeTester.java
@@ -1,8 +1,56 @@
 public class EncodeDecodeTester {
 
 	public static void main(String[] args) {
-		String[] words = {"a", "a"};
-		EncodeDecode encoderDecoder = new EncodeDecode(words);
-	}
+       
+		//Create test data
+        String[] originalList = {
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz", "0123456789",
+                "ABCDEFGHIJKLMnopqrstuvwxyz", "abc012", "!,.?'[]{}()<>",
+                "   ", "AZaz09", "abcXYZ012789.!?","A Z", "a z", "abc xyz",
+                "123+123=123+123", "567 MNO mno", "a,b,c,A,B,C,1,2,3"
+        };
+        
+        String[] expectedEncodedList = {
+                "CDEFGHIJKLMNOPQRSTUVWXYZAB", "cdefghijklmnopqrstuvwxyzab", "2345678901",
+                "CDEFGHIJKLMNOpqrstuvwxyzab", "cde234", "!,.?'[]{}()<>",
+                "   ", "CBcb21", "cdeZAB234901.!?","C B", "c b", "cde zab",
+                "345+345=345+345", "789 OPQ opq", "c,d,e,C,D,E,3,4,5"
+        };
+        
+        String[] expectedDecodedList = originalList;
 
+        //Create Encoder/Decoder Object
+		EncodeDecode encoderDecoder = new EncodeDecode(originalList);
+		
+		//Get encoded and decoded word lists
+		String[] encodedList = encoderDecoder.getEncodedList();
+		String[] decodedList = encoderDecoder.getDecodedList();
+		
+		//Check if all tests pass
+		boolean testsPassed = true;
+		
+        //Print the table
+        System.out.println( "Table of Test Results: " +
+        	"\n-----------------------------------------------------------------------------------------------" + 
+            "\nInput Word \tExpected Encoded Word \tEncoded Word \tExpected Decoded Word \tDecoded Word \tPass?");
+        for (int i = 0; i < originalList.length; i++){
+        	
+        	//Check if test passed
+        	boolean testPassed = ( expectedEncodedList[i].equals(encodedList[i]) ) && ( expectedDecodedList[i].equals(decodedList[i]) );
+        	testsPassed = testsPassed && testPassed;
+           
+        	System.out.print( originalList[i] + "\t");
+            System.out.print( expectedEncodedList[i] + "\t");
+            System.out.print( encodedList[i] + "\t");
+            System.out.print( expectedDecodedList[i] + "\t");
+            System.out.print( decodedList[i] + "\t");
+            System.out.println( testPassed ? "Passed" : "Failed" );
+            
+        }
+        
+        System.out.println( "-----------------------------------------------" +
+        		"------------------------------------------------");
+        System.out.println(testsPassed ? "All Tests Passed" : "One or More Tests Failed" );
+        
+	}
 }
