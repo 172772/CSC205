d63e9af4e268493fae132e02d2391b3d874ee24b
Matt Andre <matt@mattandre.me>
2012-09-07 14:00:47 -0400

    Start to fill out some of the code. Created EncodeDecode Tester.
    Signed-off-by: Matt Andre <matt@mattandre.me>

--- Lab01/src/EncodeDecode.java
+++ Lab01/src/EncodeDecode.java
@@ -11,38 +11,42 @@ public class EncodeDecode {
 	private String[] decodedList;
 	
 	//------------------------------------------
-	public void EncodeDecode(String[] originalList) {
-
+	public EncodeDecode(String[] originalList) {
+		this.originalList = originalList;
 	}
 
 	//------------------------------------------
 	public String encode(String originalWord) {
 
+		return " ";
 	}
 	
 	//------------------------------------------
 	public String decode(String codedWord) {
-
+		
+		return " ";
 	}
 	
 	//------------------------------------------
 	public char forwardMap(char ch) {
-
+		
+		return 'a';
 	}
 	
 	//------------------------------------------
 	public char backMap(char ch) {
-
+		
+		return 'a';
 	}
 	
 	//------------------------------------------
 	public String[] getEncodedList() {
-
+		return encodedList;
 	}
 	
 	//------------------------------------------
 	public String[] getDecodedList() {
-
+		return decodedList;
 	}
 }
  
---
+++ Lab01/src/EncodeDecodeTester.java
@@ -0,0 +1,8 @@
+public class EncodeDecodeTester {
+
+	public static void main(String[] args) {
+		String[] words = {"a", "a"};
+		EncodeDecode encoderDecoder = new EncodeDecode(words);
+	}
+
+}
