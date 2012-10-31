129b34e518ceccfe69fceca2fabb363a327cb7c2
Matt Andre <matt@mattandre.me>
2012-10-30 23:11:04 -0400

    Finished Lab08
    Signed-off-by: Matt Andre <matt@mattandre.me>

--- Lab08/src/IntegerList.java
+++ Lab08/src/IntegerList.java
@@ -1,10 +1,39 @@
 import linkedlist.DoublyLinkedList;
 
+//---------------------------------------------------
+
+/**
+ * Holds methods for a list of integers.
+ * 
+ * @author Matt Andre, Riley Bauer
+ * 
+ */
 public class IntegerList {
+
+	// ---------------------------------------------------
+
+	/**
+	 * Returns largest integer in list. Returns min integer value if list is
+	 * empty.
+	 * 
+	 * @param integer list
+	 * @return largest integer
+	 */
 	public int largest(DoublyLinkedList<Integer> integerList) {
+
+		// Check if list is empty
+		if (integerList.isEmpty()) {
+			System.out.println("Error - List is empty");
+			return Integer.MIN_VALUE;
+		}
+
+		// Reset current element of list
 		integerList.resetCurrentElement();
+
+		// Set largest element to first
 		int largest = integerList.nextElement();
 
+		// Check if there is a larger element in list
 		while (integerList.hasMoreElements()) {
 			int next = integerList.nextElement();
 			if (largest < next) {
@@ -15,23 +44,54 @@ public class IntegerList {
 		return largest;
 	}
 
+	// ---------------------------------------------------
+
+	/**
+	 * Returns number of even numbers in list
+	 * 
+	 * @param integer list
+	 * @return number of even
+	 */
 	public int countEven(DoublyLinkedList<Integer> integerList) {
+
+		// Set number of even to 0
 		int numEven = 0;
+
+		// Reset current element in list
 		integerList.resetCurrentElement();
+
+		// Check each item in list to see if it is even
 		while (integerList.hasMoreElements()) {
 			if (integerList.nextElement() % 2 == 0) {
 				numEven++;
 			}
 		}
+
 		return numEven;
 	}
 
+	// ---------------------------------------------------
+
+	/**
+	 * Returns the sum of the numbers in the list
+	 * 
+	 * @param integer list
+	 * @return sum
+	 */
 	public int sum(DoublyLinkedList<Integer> integerList) {
+
+		// Set sum to 0
 		int sum = 0;
+
+		// Reset current element in list
 		integerList.resetCurrentElement();
+
+		// Add each item in list to sum
 		while (integerList.hasMoreElements()) {
 			sum += integerList.nextElement();
 		}
+
 		return sum;
 	}
+
 }
--- Lab08/src/IntegerListTester.java
+++ Lab08/src/IntegerListTester.java
@@ -1,11 +1,28 @@
 import linkedlist.DoublyLinkedList;
+//---------------------------------------------------
 
-
+/**
+ * Tester Class for Integer List
+ * @author Matt Andre, Riley Bauer
+ */
 public class IntegerListTester {
-
+	//---------------------------------------------------
+	
+	/**
+	 * Main Method for tester
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		
+		// Create Integer list
 		IntegerList integerList = new IntegerList();
+		
+		//---------------------------------------------------
+		
+		// Create linked list of integers
 		DoublyLinkedList<Integer> integers1 = new DoublyLinkedList<Integer>();
+		
+		// Fill list
 		integers1.insertAtEnd(1);
 		integers1.insertAtEnd(2);
 		integers1.insertAtEnd(3);
@@ -14,11 +31,47 @@ public class IntegerListTester {
 		integers1.insertAtEnd(6);
 		integers1.insertAtEnd(7);
 		integers1.insertAtEnd(8);
-		integers1.insertAtEnd(9);
-		integers1.insertAtEnd(10);
-		System.out.println("Largest=" + integerList.largest(integers1));
-		System.out.println("NumEven=" + integerList.largest(integers1));
-
+		
+		// Output calculations
+		System.out.println("List = { 1, 2, 3, 4, 5, 6, 7, 8 }");
+		System.out.println("Largest = " + integerList.largest(integers1));
+		System.out.println("Number of Even = " + integerList.countEven(integers1));
+		System.out.println("Sum = " + integerList.sum(integers1));
+		System.out.println();
+		
+		//---------------------------------------------------
+		
+		// Create linked list of integers
+		DoublyLinkedList<Integer> integers2 = new DoublyLinkedList<Integer>();
+		
+		// Fill list
+		integers2.insertAtEnd(-3);
+		integers2.insertAtEnd(1);
+		integers2.insertAtEnd(0);
+		integers2.insertAtEnd(-2);
+		integers2.insertAtEnd(2);
+		integers2.insertAtEnd(3);
+		integers2.insertAtEnd(1);
+		integers2.insertAtEnd(3);
+		
+		// Output calculations
+		System.out.println("List = { -3, 1, 0, -2, 2, 3, 1, 3 }");
+		System.out.println("Largest = " + integerList.largest(integers2));
+		System.out.println("Number of Even = " + integerList.countEven(integers2));
+		System.out.println("Sum = " + integerList.sum(integers2));
+		System.out.println();
+		
+		//---------------------------------------------------
+		
+		// Create linked list of integers
+		DoublyLinkedList<Integer> integers3 = new DoublyLinkedList<Integer>();
+		
+		// Output calculations
+		System.out.println("List = { }");
+		System.out.println("Largest = " + integerList.largest(integers3));
+		System.out.println("Number of Even = " + integerList.countEven(integers3));
+		System.out.println("Sum = " + integerList.sum(integers3));
+		System.out.println();	
 	}
 
 }
